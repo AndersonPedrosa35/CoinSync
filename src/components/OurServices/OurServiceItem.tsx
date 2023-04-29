e08b225f3cdab745d16ec4d3f0ea04739461ca8c
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2023-04-29 20:16:56 -0300

    Feat: Implementa a seção de serviços na home

--- src/components/OurServices/OurServiceItem.tsx
+++ src/components/OurServices/OurServiceItem.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
+import style from './style.module.scss'
 
 interface OurServiceItemProps {
   image?: string
   title: string
   description: string
   who: string
+  className?: string
 }
 
-export default function OurServiceItem({ image, title, description, who }: OurServiceItemProps) {
+export default function OurServiceItem({ image, title, description, who, className }: OurServiceItemProps) {
   return (
-    <section>
+    <section className={`${style.containerOurServiceItem} ${className && style[className]}`}>
       { image && ( <img src={image} alt={title} /> ) }
-      <h3>{ who }</h3>
-      <h1>{ title }</h1>
-      <p>{description}</p>
+      <h3 className={style.who}>{ who }</h3>
+      <h1 className={style.title}>{ title }</h1>
+      <p className={style.description}>{description}</p>
     </section>
   )
 }
--- src/components/OurServices/index.tsx
+++ src/components/OurServices/index.tsx
@@ -29,16 +29,30 @@ const items = [
   }
 ]
 
+function arrayElementShouldCapture(array: Array<any>) {
+  let indexCapture = []
+  for (let i = 2; i < array.length; i += 4) {
+    indexCapture.push(i);
+    indexCapture.push(i+1);
+  }
+  return indexCapture
+}
+
 export default function OurServices() {
+  const indexCapture = arrayElementShouldCapture(items)
   return (
     <section className={style.containerOurServices}>
-      <section>
+      <section className={style.containerLeft}>
         {items.map((item: any, index) => (
-          <OurServiceItem key={index} {...item} />
+          <OurServiceItem className={ indexCapture.includes(index) ? "spaceLeft" : '' } key={index} {...item} />
         ))}
       </section>
-      <section>
-        {/* <OurServiceItem /> */}
+      <section className={style.containerRight}>
+        <OurServiceItem
+          title={"Lorem ipsum"}
+          description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor"}
+          who={"Lorem ipsum"}
+        />
       </section>
     </section>
   )
